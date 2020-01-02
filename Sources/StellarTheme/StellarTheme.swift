5bfc299ec1840a11c4ca8c3cb51cc2e5ab7f16fa
Danilo Campos <danilo@futurefluent.com>
2020-01-01 20:16:14 -0500

    Basics

--- Sources/StellarTheme/StellarTheme.swift
+++ Sources/StellarTheme/StellarTheme.swift
@@ -1,3 +1,69 @@
-struct StellarTheme {
-    var text = "Hello, World!"
+//
+//  StellarTheme.swift
+//
+//  Created by Danilo Campos on 1/1/20.
+//
+
+import Foundation
+import Publish
+import Plot
+
+public extension Theme {
+    
+    static var stellar: Self {
+        Theme(
+            htmlFactory: StellarHTMLFactory(),
+            resourcePaths: ["Resources/"]
+        )
+    }
+}
+
+private struct StellarHTMLFactory<Site: Website>: HTMLFactory {
+    func makeSectionHTML(for section: Section<Site>, context: PublishingContext<Site>) throws -> HTML {
+        HTML()
+    }
+    
+    func makeItemHTML(for item: Item<Site>, context: PublishingContext<Site>) throws -> HTML {
+        HTML()
+    }
+    
+    func makePageHTML(for page: Page, context: PublishingContext<Site>) throws -> HTML {
+        HTML()
+    }
+    
+    func makeTagListHTML(for page: TagListPage, context: PublishingContext<Site>) throws -> HTML? {
+        HTML()
+    }
+    
+    func makeTagDetailsHTML(for page: TagDetailsPage, context: PublishingContext<Site>) throws -> HTML? {
+        HTML()
+    }
+    
+    fileprivate func makeIndexHTML(for index: Index,
+                                   context: PublishingContext<Site>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: index, on: context.site),
+            .body(
+                .div(
+                    .class("wrapper"),
+                    .header(
+                        .span(.class("logo"),
+                              .img(.src(context.site.imagePath ?? ""))
+                        ),
+                        .h1(.text(index.title)),
+                        .p(
+                            .class("description"),
+                            .text(context.site.description)
+                        )
+                    )
+                    
+                )
+            )
+        )
+    }
+}
+
+private extension Node where Context == HTML.BodyContext {
+    
 }
--- Tests/StellarThemeTests/StellarThemeTests.swift
+++ Tests/StellarThemeTests/StellarThemeTests.swift
@@ -6,7 +6,6 @@ final class StellarThemeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StellarTheme().text, "Hello, World!")
     }
 
     static var allTests = [
